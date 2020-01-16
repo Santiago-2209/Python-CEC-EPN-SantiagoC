27ab9b23d2fbd2baf36fee27a4e6e02963474d7d
Santiago-2209 <59969805+Santiago-2209@users.noreply.github.com>
2020-01-16 11:07:07 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+ciudad=input("Ingrese su ciudad ")
+nombre=input("Ingrese su nombre ")
+apellido=input("Ingrese su apellido ")
+age=input("ingrese su edad ")
+space=" "
+print("Archivo generado por " + nombre + space + apellido + space + "con " + age + space + "Años" + space +"en " + ciudad  )
---
+++ 04_if-vlan.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 11:57:54 2020
+
+@author: CEC
+"""
+nativeVLAN=100
+dataVLAN=100
+if nativeVLAN == dataVLAN:
+    print ("The native VLAN and the data VLAN are the same.")
+else:
+    print ("The native VLAN and the data VLAN are different.")
